c55543213aba23bfb5829d3148b3efc5c23a89ee
Samuli Ristimäki <samuli.ristimaki@gmail.com>
2018-03-05 23:15:41 +0200

    Remove unused var

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Script from 'react-load-script';
+// import Script from 'react-load-script';
 // import Link from 'gatsby-link';
 
 import { TitleAndMetaTags } from '../components/TitleAndMetaTags/TitleAndMetaTags';
