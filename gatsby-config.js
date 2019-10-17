ee18a6a2937f2f304245803c8cccd507f44460b4
Samuli Ristimäki <samuli.ristimaki@frantic.com>
2019-10-17 04:30:10 +0300

    format

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: "Samuli Ristimäki",
-    author: "Samuli Ristimäki",
-    description: "Samuli Ristimäki - Portfolio"
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/64x64.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
-  ],
-}
+	siteMetadata: {
+		title: 'Samuli Ristimäki',
+		author: 'Samuli Ristimäki',
+		description: 'Samuli Ristimäki - Portfolio'
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: 'samuli-ristimäki',
+				short_name: 'samuli',
+				start_url: '/',
+				background_color: '#663399',
+				theme_color: '#663399',
+				display: 'minimal-ui',
+				icon: 'src/images/64x64.png' // This path is relative to the root of the site.
+			}
+		},
+		'gatsby-plugin-sass',
+		'gatsby-plugin-offline'
+	]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,120 +1,121 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from 'react';
+import Layout from '../components/layout';
 
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer'
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
 
-import { withPrefix } from 'gatsby'
+import { withPrefix } from 'gatsby';
 
 class IndexPage extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isArticleVisible: false,
-      timeout: false,
-      articleTimeout: false,
-      article: '',
-      loading: 'is-loading'
-    }
-    this.handleOpenArticle = this.handleOpenArticle.bind(this)
-    this.handleCloseArticle = this.handleCloseArticle.bind(this)
-    this.setWrapperRef = this.setWrapperRef.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isArticleVisible: false,
+			timeout: false,
+			articleTimeout: false,
+			article: '',
+			loading: 'is-loading'
+		};
+		this.handleOpenArticle = this.handleOpenArticle.bind(this);
+		this.handleCloseArticle = this.handleCloseArticle.bind(this);
+		this.setWrapperRef = this.setWrapperRef.bind(this);
+		this.handleClickOutside = this.handleClickOutside.bind(this);
+	}
 
-  componentDidMount () {
-    this.timeoutId = setTimeout(() => {
-        this.setState({loading: ''});
-    }, 100);
-    document.addEventListener('mousedown', this.handleClickOutside);
-  }
+	componentDidMount() {
+		this.timeoutId = setTimeout(() => {
+			this.setState({ loading: '' });
+		}, 100);
+		document.addEventListener('mousedown', this.handleClickOutside);
+	}
 
-  componentWillUnmount () {
-    if (this.timeoutId) {
-        clearTimeout(this.timeoutId);
-    }
-    document.removeEventListener('mousedown', this.handleClickOutside);
-  }
+	componentWillUnmount() {
+		if (this.timeoutId) {
+			clearTimeout(this.timeoutId);
+		}
+		document.removeEventListener('mousedown', this.handleClickOutside);
+	}
 
-  setWrapperRef(node) {
-    this.wrapperRef = node;
-  }
+	setWrapperRef(node) {
+		this.wrapperRef = node;
+	}
 
-  handleOpenArticle(article) {
+	handleOpenArticle(article) {
+		this.setState({
+			isArticleVisible: !this.state.isArticleVisible,
+			article
+		});
 
-    this.setState({
-      isArticleVisible: !this.state.isArticleVisible,
-      article
-    })
+		setTimeout(() => {
+			this.setState({
+				timeout: !this.state.timeout
+			});
+		}, 325);
 
-    setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
+		setTimeout(() => {
+			this.setState({
+				articleTimeout: !this.state.articleTimeout
+			});
+		}, 350);
+	}
 
-    setTimeout(() => {
-      this.setState({
-        articleTimeout: !this.state.articleTimeout
-      })
-    }, 350)
+	handleCloseArticle() {
+		this.setState({
+			articleTimeout: !this.state.articleTimeout
+		});
 
-  }
+		setTimeout(() => {
+			this.setState({
+				timeout: !this.state.timeout
+			});
+		}, 325);
 
-  handleCloseArticle() {
+		setTimeout(() => {
+			this.setState({
+				isArticleVisible: !this.state.isArticleVisible,
+				article: ''
+			});
+		}, 350);
+	}
 
-    this.setState({
-      articleTimeout: !this.state.articleTimeout
-    })
+	handleClickOutside(event) {
+		if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+			if (this.state.isArticleVisible) {
+				this.handleCloseArticle();
+			}
+		}
+	}
 
-    setTimeout(() => {
-      this.setState({
-        timeout: !this.state.timeout
-      })
-    }, 325)
-
-    setTimeout(() => {
-      this.setState({
-        isArticleVisible: !this.state.isArticleVisible,
-        article: ''
-      })
-    }, 350)
-
-  }
-
-  handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      if (this.state.isArticleVisible) {
-        this.handleCloseArticle();
-      }
-    }
-  }
-
-  render() {
-    return (
-      <Layout location={this.props.location}>
-        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
-          <div className="canvas">
-            <canvas></canvas>
-          </div>
-          <div id="wrapper">
-            <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
-            <Main
-              isArticleVisible={this.state.isArticleVisible}
-              timeout={this.state.timeout}
-              articleTimeout={this.state.articleTimeout}
-              article={this.state.article}
-              onCloseArticle={this.handleCloseArticle}
-              setWrapperRef={this.setWrapperRef}
-            />
-            <Footer timeout={this.state.timeout} />
-            
-          </div>
-        </div>
-      </Layout>
-    )
-  }
+	render() {
+		return (
+			<Layout location={this.props.location}>
+				<div
+					className={`body ${this.state.loading} ${
+						this.state.isArticleVisible ? 'is-article-visible' : ''
+					}`}>
+					<div className="canvas">
+						<canvas id="canvas"></canvas>
+					</div>
+					<div id="wrapper">
+						<Header
+							onOpenArticle={this.handleOpenArticle}
+							timeout={this.state.timeout}
+						/>
+						<Main
+							isArticleVisible={this.state.isArticleVisible}
+							timeout={this.state.timeout}
+							articleTimeout={this.state.articleTimeout}
+							article={this.state.article}
+							onCloseArticle={this.handleCloseArticle}
+							setWrapperRef={this.setWrapperRef}
+						/>
+						<Footer timeout={this.state.timeout} />
+					</div>
+				</div>
+			</Layout>
+		);
+	}
 }
 
-export default IndexPage
+export default IndexPage;
