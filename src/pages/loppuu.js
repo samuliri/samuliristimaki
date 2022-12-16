b5a9a4aa46f42d0dc27551fbee15c3173c83c5b8
samuliristimaki <samuli.ristimaki@reaktor.com>
2022-12-16 11:30:13 +0200

    Add loppuu

---
+++ src/pages/loppuu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ThirdPage = () => (
+	<div style={{ width: '100vw', height: '100vh' }}>
+		<iframe src="https://my.spline.design/dominoeffectphysicscopy-5e43606e154ab3848dbb3b6734b59f98/" width="100%" height="100%"></iframe>
+	</div>
+);
+
+export default ThirdPage;
